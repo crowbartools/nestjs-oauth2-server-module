07d51e069790280bc9aeab12893a9e548fec82e2
Erik Bigler <erikbigler@gmail.com>
2020-11-22 19:31:47 -0600

    Include dist

---
+++ dist/domain/access-token.entity.d.ts
@@ -0,0 +1,18 @@
+import { ObjectID } from 'typeorm';
+import { ClientEntity } from './client.entity';
+import { OAuth2Request } from '../ui';
+export declare class AccessTokenEntity {
+    id: ObjectID;
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpiresAt: Date;
+    refreshTokenExpiresAt: Date;
+    client: ClientEntity;
+    userId: string;
+    /**
+     * JSON List of api IDs granted with this token for the client
+     */
+    scope: string;
+    createdAt: Date;
+    createdFrom: OAuth2Request;
+}
---
+++ dist/domain/client.entity.d.ts
@@ -0,0 +1,21 @@
+import { ObjectID } from 'typeorm';
+export declare class ClientEntity {
+    id: ObjectID;
+    name: string;
+    clientId: string;
+    clientSecret: string;
+    grants: string[];
+    /**
+     * Client scope. The scope should contain the list of third party applications
+     * the client has access to
+     */
+    scope: string;
+    accessTokenLifetime: number;
+    refreshTokenLifetime: number;
+    privateKey: string;
+    publicKey: string;
+    cert: string;
+    certExpiresAt: Date;
+    createdAt: Date;
+    deletedAt: Date;
+}
